de0bcdf729268f3388c423cd139b7dadbcb588f4
Eyüp <epsilonelittr@gmail.com>
2025-07-05 04:33:41 +0300

    Setup vue app

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-import './style.scss'
+import "./style.scss";
 
-import './demos/ipc'
+// import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
-createApp(App)
-  .mount('#app')
-  .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+
+app.mount("#app").$nextTick(() => {
+  postMessage({ payload: "removeLoading" }, "*");
+});
